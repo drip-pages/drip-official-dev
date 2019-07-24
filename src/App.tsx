ad6f9244dbcb6eb905c482dea8ea242c0c9b2585
fukui_takanori <itfukui0922@icloud.com>
2019-07-24 18:17:01 +0900

    add: ルーティングが動作することをテストするためにテスト処理を書いた

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,46 @@
 import React from 'react';
-import './App.css';
+import './App.scss';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
+
 import Header from './components/Header'
 
 function App() {
   return (
     <div className="App">
-      <Header />
+      <BrowserRouter>
+        <div>
+          <Route exact path='/' component={Home} />
+          <Route path='/about' component={About} />
+          <Route path='/friends' component={Friends} />
+        </div>
+      </BrowserRouter>
     </div>
-  );
+  )
 }
+
+const Home = () => (
+    <div>
+        <h2>
+            Home
+        </h2>
+        <p>
+            Welcome Home.
+        </p>
+    </div>
+)
+
+const About = () => (
+    <div>
+        <h2>About</h2>
+        <p>フレンズに投票するページです</p>
+    </div>
+)
+
+const Friends = () => (
+    <div>
+        <h2>Friends</h2>
+        <p>ここにフレンズのリストを書きます</p>
+    </div>
+)
 
 export default App;
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -6,6 +6,7 @@ class Header extends React.Component {
         return (
             <div className="Header">
                 <img alt="DRECOM INVENTION PROJECT" src={headerLogo} className = "header-logo" />
+            {/* menuボタン（コンポーネント化）を追加する。その後、リストをここに追加する*/}
             </div>
         )
     }
