31743ab122b6022276eeb9a1e2f7a8e07b8ff056
fukui_takanori <itfukui0922@icloud.com>
2019-08-08 13:39:52 +0900

    up: ヘッダー部のcssを編集

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,67 +1,64 @@
 import React from 'react'
 import headerLogo from '../../img/navi.png'
 import './Header.scss'
-
+import Tabs from "@material-ui/core/Tabs";
+import Tab from "@material-ui/core/Tab";
 
 type HeaderState = {
     showMenu: boolean,
+    value: number,
 }
 class Header extends React.Component<{}, HeaderState> {
     state ={
         showMenu: false,
+        value: 0,
     }
 
     handleMenuButton = () => {
         this.state.showMenu ? this.setState({ showMenu: false },) : this.setState({ showMenu: true })
     }
 
-    handleNewsButton = () => {
-
-    }
-
-    handleCompanyButton = () => {
-
-    }
-
-    handleContactButton = () => {
-
-    }
-
-    handleEnglishButton = () => {
-
+    handleChange = (event: React.ChangeEvent<{}>, newValue: number) =>{
+        this.setState({
+            value: newValue,
+        })
+        console.log(`newValue ${newValue}`)
     }
 
     render() {
         return (
             <div className="Header">
-                <div id="fixed-area-id" className="fixed-area">
-                    <img alt="DRECOM INVENTION PROJECT" src={headerLogo} className="header-logo" />
-                    <span className="menu-area">
-                        <span className="native-button">
-                            <button onClick={this.handleMenuButton}>
-                                メニューボタン
-                            </button>
-                        </span>
-                        <span className="no-native-buttons">
-                            <button onClick={this.handleNewsButton}>
-                                News
-                            </button>
-                            <button onClick={this.handleCompanyButton}>
-                                Company
-                            </button>
-                            <button onClick={this.handleContactButton}>
-                                Contact
-                            </button>
-                            <button onClick={this.handleEnglishButton}>
-                                English
-                            </button>
-                        </span>
+                <img alt="DRECOM INVENTION PROJECT" src={headerLogo} className="header-logo" />
+                <span className="menu-area">
+                    <span className="native-button">
+                        <button onClick={this.handleMenuButton}>
+                            メニューボタン
+                        </button>
                     </span>
-                </div>
-
+                    <Tabs
+                        className="no-native-buttons"
+                        value={this.state.value}
+                        indicatorColor="primary"
+                        textColor="primary"
+                        onChange={this.handleChange}
+                        aria-label="menu"
+                    >
+                        <Tab label="News" />
+                        <Tab label="Company" />
+                        <Tab label="Contact" />
+                        <Tab label="English" />
+                    </Tabs>
+                </span>
             </div>
         )
     }
 }
+
+// const a11yProps = (index: number) => {
+//     return {
+//         id: `simple-tab-${index}`,
+//         'aria-controls': `simple-tabpanel-${index}`,
+//     }
+// }
 
 export default Header
