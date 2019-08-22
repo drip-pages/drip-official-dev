dd900a86a0f8fbdab75e8559ba51eebe7ea7f7ab
fukui_takanori <itfukui0922@icloud.com>
2019-08-22 17:46:25 +0900

    add: memberの上にマウスが来た時にアニメーションするように

--- src/components/Member/index.tsx
+++ src/components/Member/index.tsx
@@ -1,19 +1,30 @@
-import * as React from 'react'
+import React, {useState} from 'react'
 import './Member.scss'
 import classNames from 'classnames'
 
 type MemberProps = {
     imgFile: string
+    movieFile: string
     name: string
     className?: string,
 }
-const Member = (props: MemberProps) => (
-    <div className={classNames('Member', props.className)}>
-        <img className="member-image" src={props.imgFile} alt="member" />
-        <p>
-            { props.name }
-        </p>
-    </div>
-)
+const Member = (props: MemberProps) => {
+    const [mouseOver, setMouseOver] = useState(false)
+
+    return (
+        <div className={classNames('Member', props.className)}>
+            <img
+                className="member-image"
+                src={ mouseOver ? props.movieFile : props.imgFile }
+                alt="member"
+                onMouseEnter={() => setMouseOver(true)}
+                onMouseLeave={() => setMouseOver(false)}
+            />
+            <p>
+                { props.name }
+            </p>
+        </div>
+    )
+}
 
 export default Member
