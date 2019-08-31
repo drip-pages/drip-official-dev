8109a56384e4ca3b64e91c81d6b6620a0e8232f7
fukui_takanori <itfukui0922@icloud.com>
2019-08-31 12:24:06 +0900

    add: OurInventionまで英語化v2

--- src/components/OurInvention/index.tsx
+++ src/components/OurInvention/index.tsx
@@ -17,22 +17,22 @@ const OurInvention = () => {
         <h2>Our invention</h2>
       </div>
       <Invention className="arow" bigLogo={arowBigLogo} smallLogo={arowSmallLogo}>
-        <p>3DリアルマップとPOIデータを活用し</p>
-        <p>没入感のある新しいゲーム体験を実現するためのプラットフォーム</p>
+        <p>{t('inventionArow_1')}</p>
+        <p>{t('inventionArow_2')}</p>
         <a href="https://arow.world/" target="_blank" rel="noopener noreferrer">
           https://arow.world/
         </a>
       </Invention>
       <Invention className="pillars" bigLogo={pillarsBigLogo} smallLogo={pillarsSmallLogo}>
-        <p>DAppsをまたいで利用可能なトークンを、誰でも生成できるERC721拡張スマートコントラクト</p>
+        <p>{t('inventionPillars_1')}</p>
         <a href="https://6-pillars.ooo/" target="_blank" rel="noopener noreferrer">
           https://6-pillars.ooo/
         </a>
       </Invention>
       <Invention className="loveChain" bigLogo={loveChainBigLogo} smallLogo={loveCahinSmallLogo}>
-        <strong>ブロックチェーン上に2人だけのメッセージを永遠に残すサービスです。</strong>
+        <strong>{t('inventionLove_1')}</strong>
         <p>
-          ブロックチェーン上に保存する事によって改ざんされる事なく永遠に記録する事が出来ます。2人だけの思い出をブロックチェーン上に残しましょう。
+          {t('inventionLove_2')}
         </p>
         <a href="https://lovechain.ooo/" target="_blank" rel="noopener noreferrer">
           https://lovechain.ooo/
