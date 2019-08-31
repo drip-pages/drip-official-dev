c3258ca8ad713470798659b97116c2e6de0469f3
fukui_takanori <itfukui0922@icloud.com>
2019-08-31 13:26:47 +0900

    add: ContactUsまで英語化

--- src/components/ContactUs/index.tsx
+++ src/components/ContactUs/index.tsx
@@ -1,31 +1,35 @@
 import * as React from 'react'
 import './ContactUs.scss'
+import { useTranslation } from 'react-i18next'
 
-const ContactUs = () => (
-  <div className="ContactUs">
-    <h2>Contact us!</h2>
-    <div className="contents">
-      <div className="label">Media</div>
-      <span className="message">
-        取材の申し込み、掲載、どんどん募集しています。
+const ContactUs = () => {
+  const { t } = useTranslation()
+  return (
+    <div className="ContactUs">
+      <h2>Contact us!</h2>
+      <div className="contents">
+        <div className="label">Media</div>
+        <span className="message">
+          {t('contactMediaMessage')}
+          <br />
+        </span>
+        <div className="label">Partner</div>
+        <span className="message">
+          {t('contactPartnerMessage')}
+          <br />
+        </span>
+        <div className="label">Recruit</div>
+        <span className="message">
+          {t('contactRecruitMessage')}
+          <br />
+        </span>
         <br />
-      </span>
-      <div className="label">Partner</div>
-      <span className="message">
-        何か面白いことを一緒にやりましょう！
-        <br />
-      </span>
-      <div className="label">Media</div>
-      <span className="message">
-        我こそは！と思う熱気にあふれた若者を待っています！
-        <br />
-      </span>
-      <br />
+      </div>
+      <a href="https://goo.gl/forms/my00T6ZbZK" target="_blank" rel="noopener noreferrer">
+        <button className="contact-button">{t('contactButton')}</button>
+      </a>
     </div>
-    <a href="https://goo.gl/forms/my00T6ZbZK" target="_blank" rel="noopener noreferrer">
-      <button className="contact-button">お問い合わせ</button>
-    </a>
-  </div>
-)
+  )
+}
 
 export default ContactUs
