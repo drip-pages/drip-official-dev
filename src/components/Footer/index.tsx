c110451d15bf384cbcbf6cf20687f652acf4bfa9
fukui_takanori <itfukui0922@icloud.com>
2019-09-01 14:05:47 +0900

    add: 微調整　SNSボタンリンク追加

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -3,16 +3,29 @@ import './Footer.scss'
 import drecomLogo from '../../img/drecom.png'
 import facebookIcon from '../../img/fb.png'
 import twitterIcon from '../../img/tw.png'
+import Slide from '../Slide';
 
 const Footer = () => (
-  <div className="Footer">
+  <Slide className="Footer">
     <img className="drecom-logo" alt="drecom logo" src={drecomLogo} />
     <div className="sns-icons">
-      <img className="facebook icon" alt="facebook" src={facebookIcon} />
-      <img className="twitter icon" alt="twitter" src={twitterIcon} />
+      <a
+        href="https://www.facebook.com/sharer/sharer.php?u=http%3A%2F%2Fdrip.drecom.co.jp"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <img className="facebook icon" alt="facebook" src={facebookIcon} />
+      </a>
+      <a
+        href="http://twitter.com/intent/tweet?url=http%3A%2F%2Fdrip.drecom.co.jp&amp;text=DRIP%20-%20Drecom%20Invention%20Project&amp;tw_p=tweetbutton"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <img className="twitter icon" alt="twitter" src={twitterIcon} />
+      </a>
     </div>
     <p>Copyright 2019 Drecom Co., Ltd. All Rights Reserved.</p>
-  </div>
+  </Slide>
 )
 
 export default Footer
