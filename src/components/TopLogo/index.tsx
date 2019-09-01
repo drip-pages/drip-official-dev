b84bb42c988618e7522190448102465f80950ab6
fukui_takanori <itfukui0922@icloud.com>
2019-09-01 12:21:05 +0900

    Revert "fix: topLogoの微修正"
    This reverts commit 9e88e0b5

--- src/components/TopLogo/index.tsx
+++ src/components/TopLogo/index.tsx
@@ -2,15 +2,16 @@ import topLogo from '../../img/logo2.png'
 import * as React from 'react'
 import './TopLogo.scss'
 import { useTranslation } from 'react-i18next'
-import Slide from '../Slide';
 
 const TopLogo = () => {
   const { t } = useTranslation()
   return (
-    <Slide className="TopLogo">
-      <img className="top-logo" src={topLogo} alt="top-logo" />
+    <div className="TopLogo">
+      <div className="inner">
+        <img className="top-logo" src={topLogo} alt="top-logo" />
+      </div>
       <h1>{t('topLogoMessage')}</h1>
-    </Slide>
+    </div>
   )
 }
 
