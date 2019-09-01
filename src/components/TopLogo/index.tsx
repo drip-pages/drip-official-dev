9e88e0b5fd4bc3a56631cf987f4e8b6ca2aca624
fukui_takanori <itfukui0922@icloud.com>
2019-09-01 10:08:50 +0900

    fix: topLogoの微修正

--- src/components/TopLogo/index.tsx
+++ src/components/TopLogo/index.tsx
@@ -2,16 +2,15 @@ import topLogo from '../../img/logo2.png'
 import * as React from 'react'
 import './TopLogo.scss'
 import { useTranslation } from 'react-i18next'
+import Slide from '../Slide';
 
 const TopLogo = () => {
   const { t } = useTranslation()
   return (
-    <div className="TopLogo">
-      <div className="inner">
-        <img className="top-logo" src={topLogo} alt="top-logo" />
-      </div>
+    <Slide className="TopLogo">
+      <img className="top-logo" src={topLogo} alt="top-logo" />
       <h1>{t('topLogoMessage')}</h1>
-    </div>
+    </Slide>
   )
 }
 
