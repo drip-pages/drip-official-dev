ef63283da1377117e4ae81a1fbe87885e46ef487
fukui_takanori <itfukui0922@icloud.com>
2019-09-01 13:03:25 +0900

    add: topLogo slide適用

--- src/components/Slide/index.tsx
+++ src/components/Slide/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import classNames from 'classnames'
+import './Slide.scss'
 
 type slidePorps = {
   children?: JSX.Element[]
--- src/components/TopLogo/index.tsx
+++ src/components/TopLogo/index.tsx
@@ -2,16 +2,17 @@ import topLogo from '../../img/logo2.png'
 import * as React from 'react'
 import './TopLogo.scss'
 import { useTranslation } from 'react-i18next'
+import Slide from '../Slide'
 
 const TopLogo = () => {
   const { t } = useTranslation()
   return (
-    <div className="TopLogo">
+    <Slide className="TopLogo">
       <div className="inner">
         <img className="top-logo" src={topLogo} alt="top-logo" />
       </div>
       <h1>{t('topLogoMessage')}</h1>
-    </div>
+    </Slide>
   )
 }
 
